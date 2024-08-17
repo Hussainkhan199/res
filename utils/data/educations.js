a864544c75d9ae9675a7c65d0f592b422c5bc70d
hussainkhan199 <mk5558959@gmail.com>
2024-08-17 21:01:47 +0500

    "Updated education details: changed durations and institutions for all three educations."

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2018 - 2024",
+    institution: "Abdul Wali Khan University Mardan",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2016 - 2028",
+    institution: "Govt Post Graduate College Mardan",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2014 - 2016",
+    institution: "Mardan Model School and College Mardan",
   }
 ]
