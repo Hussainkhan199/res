a8f9d5ef97e6fb8e1f5ec6495ff5258076f7c587
hussainkhan199 <mk5558959@gmail.com>
2024-08-17 21:32:06 +0500

    s

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Muhammad Hussain",
-  profile: '/profile.jpg',
+  profile: '/profile.png',
   designation: "Full Stack Developer",
   description: "My name is Muhammad Hussain. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript, PHP and Wordpress. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'mhussain.programmer@gmail.com',
